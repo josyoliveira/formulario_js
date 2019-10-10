0dcaec12bd472cc2b3e56c64170474a1ba003763
josyoliveira <joseane.olliveira52@gmail.com>
2019-10-09 22:52:46 -0300

    Foi inserido a validacao denome e email

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function limparForm(){
     document.getElementById('myForm').reset();
 }
+
+function validarForm(){
+    if(document.getElementById('nome').value == "" || document.getElementById('nome').value.lenght < 3){
+        alert("Insira seu nome com pelo menos três letras");
+        document.getElementById('nome').focus();
+
+        return false;
+    }
+
+    if(document.getElementById('email').value == "" || document.getElementById('email').value.indexOf('@') == -1
+        || document.getElementById('email').value.indexOf('.') == -1){
+            alert("Preencha o seu E-MAIL corretamente");
+            document.getElementById('nome').focus();
+
+            return false;
+        }
+
+}
