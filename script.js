581ba0832666f601ef44ef4777ee142ed158944d
josyoliveira <joseane.olliveira52@gmail.com>
2019-10-11 21:37:19 -0300

    Adicao de validacao de senha

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function limparForm(){
 }
 
 function validarForm(){
-    if(document.getElementById('nome').value == "" || document.getElementById('nome').value.lenght < 3){
+
+    var nome = document.getElementById('nome').value;
+    var senha = document.getElementById('senha').value;
+    var confirSenha = document.getElementById('confirSenha').value;
+    
+    if(document.getElementById('nome').value == "" || nome.length < 3){
         alert("Insira seu nome com pelo menos três letras");
         document.getElementById('nome').focus();
 
@@ -17,5 +22,22 @@ function validarForm(){
 
             return false;
         }
+    if(document.getElementById('senha').value == "" || senha.length < 6){
+        alert("A senha deve ter pelo menos 6 caracteres");
+        document.getElementById('senha').focus();
+
+        return false;
+    }
+
+    if (document.getElementById('confirSenha').value == "" || confirSenha.length < 6) {
+        alert('Preencha o campo repetir senha.');
+        document.getElementById('confirSenha').focus();
+    }
+
+    if(senha != confirSenha){
+        alert("Sua senha está diferente do campo anterior.");
+        document.getElementById('confirSenha').focus();
+        return false;
+    }
 
 }
